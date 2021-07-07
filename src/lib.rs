5cea866f82263a7ae09ef633cdde982b6fbf2670
Søren Alsbjerg Hørup <sah@sah.dk>
2021-07-07 06:31:41 +0200

    added canvas resize, sort of

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, window::WindowMode};
+use bevy::{prelude::*};
 use wasm_bindgen::prelude::*;
 mod systems;
 pub use systems::*;
@@ -8,23 +8,25 @@ pub fn all(app: &mut AppBuilder) {
     .add_plugins(DefaultPlugins)
     .add_startup_system(spawn_ui.system())
     .add_startup_system(spawn_sprites.system())
-    ;
+    .insert_resource(WindowDescriptor {
+        width:1024.,
+        height:786.,
+        ..Default::default()
+    });
 }
 
 #[cfg(target_arch = "wasm32")]
 pub fn web_only(app: &mut AppBuilder) {
     app.add_plugin(bevy_webgl2::WebGL2Plugin);
-    app.insert_resource(WindowDescriptor {
-        canvas:Some("main".into()),
-        ..Default::default()
-    });
 }
 
 #[cfg(not(target_arch = "wasm32"))]
 pub fn native_only(app: &mut AppBuilder) {
-    app.insert_resource(WindowDescriptor {
-        ..Default::default()
-    });
+   
+}
+
+#[cfg(target_arch = "wasm32")]
+pub fn wasm_test(wd:Res<WindowDescriptor>) {
 }
 
 #[wasm_bindgen]
@@ -32,15 +34,14 @@ pub fn start() {
     let mut app = App::build();
     
     all(&mut app);
-
-
     
     #[cfg(target_arch = "wasm32")]
     web_only(&mut app);
     #[cfg(not(target_arch = "wasm32"))]
     native_only(&mut app);
 
-
+    #[cfg(target_arch = "wasm32")]
+    app.add_system(wasm_test.system());
 
     app.run();
 }
