49eff0511090c7e996d2896b5959c13feac0ef03
Søren Alsbjerg Hørup <sah@sah.dk>
2021-07-09 09:34:00 +0200

    fixed gitingore

--- release_web.bat
+++ release_web.bat
@@ -4,5 +4,10 @@ rmdir dist-web /s /q 2>nul
 mkdir dist-web
 
 xcopy index.html dist-web\
-xcopy pkg\*.* dist-web\pkg\
+
+xcopy pkg\*.js dist-web\pkg\
+xcopy pkg\*.wasm dist-web\pkg\
+xcopy pkg\*.d.ts dist-web\pkg\
+xcopy pkg\*.wasm.d.ts dist-web\pkg\
+
 xcopy assets\*.* dist-web\assets\
